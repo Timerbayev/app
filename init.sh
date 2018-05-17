93e75df0f34f030b57c8789cb6e5b1b017d7e19b
Timerbayev <account_service@bk.ru>
2018-05-17 22:56:49 +0300

    django

--- init.sh
+++ init.sh
@@ -1,4 +1,5 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
-gunicorn -c /home/box/web/etc/django.py wsgi
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
+sudo ln -sf /home/box/web/etc/django.conf /etc/gunicorn.d/test
+sudo /etc/init.d/gunicorn restart
