cb2f79615471a1952364d7e1584d54b0382fd20a
Timerbayev <account_service@bk.ru>
2018-05-19 14:50:33 +0300

    19.05.2018

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -18,10 +18,10 @@ from django.conf.urls import url
 from qa.views import test
 
 urlpatterns = [
-url(r'^login/.*$', test, name='login'),
-url(r'^signup/.*$', test, name='signup'),
-url(r'^question/(\d+)/$', test, name='question'),
-url(r'^ask/.*$', test, name='ask'),
-url(r'^popular/.*$', test, name='popular'),
-url(r'^new/.*$', test, name='new')]
+url(r'^login/.*', test, name='login'),
+url(r'^signup/.*', test, name='signup'),
+url(r'^question/(\d+)/', test, name='question'),
+url(r'^ask/.*', test, name='ask'),
+url(r'^popular/.*', test, name='popular'),
+url(r'^new/.*', test, name='new')]
 
